cb862351fea3407e196d589f9e44025379917c63
MysticRyuujin <mysticryuujin@protonmail.com>
2021-05-03 13:56:04 -0500

    Move setup echo inside setup if statement. Move bor commands to bor entrypoint.sh

--- build/bor/scripts/entrypoint.sh
+++ build/bor/scripts/entrypoint.sh
@@ -1,13 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 
 # exit script on any error
 set -e
-echo "setting up initial configurations"
 
+# Set Bor Home Directory
 BOR_HOME=/datadir
 
 if [ ! -f "$BOR_HOME/genesis.json" ];
 then
+    echo "setting up initial configurations"
     cd $BOR_HOME
 
     echo "downloading launch genesis file"
@@ -16,3 +17,24 @@ then
     echo "initializing bor with genesis file"
     bor --datadir /datadir init /datadir/genesis.json
 fi
+
+bor \
+    --port=30303 \
+    --maxpeers=200 \
+    --datadir=/datadir \
+    --networkid=137 \
+    --miner.gaslimit=200000000 \
+    --miner.gastarget=20000000 \
+    --bor.heimdall=http://heimdallr:1317 \
+    --http \
+    --http.addr=0.0.0.0 \
+    --http.port=8545 \
+    --http.api=eth,net,web3,bor \
+    --http.corsdomain="*" \
+    --http.vhosts="*" \
+    --ws \
+    --ws.addr=0.0.0.0 \
+    --ws.port=8545 \
+    --ws.api=eth,net,web3,bor \
+    --ws.origins="*" \
+    --bootnodes=enode://0cb82b395094ee4a2915e9714894627de9ed8498fb881cec6db7c65e8b9a5bd7f2f25cc84e71e89d0947e51c76e85d0847de848c7782b13c0255247a6758178c@44.232.55.71:30303,enode://88116f4295f5a31538ae409e4d44ad40d22e44ee9342869e7d68bdec55b0f83c1530355ce8b41fbec0928a7d75a5745d528450d30aec92066ab6ba1ee351d710@159.203.9.164:30303
--- build/heimdall/scripts/entrypoint.sh
+++ build/heimdall/scripts/entrypoint.sh
@@ -1,14 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 
 # exit script on any error
 set -e
-echo "setting up initial configurations"
 
+# Set Heimdall Home Directory
 HEIMDALLD_HOME=/root/.heimdalld
 
 if [ ! -f "$HEIMDALLD_HOME/config/config.toml" ];
 then
-    heimdalld init ${MONIKER:-nonamenode}
+    echo "setting up initial configurations"
+    NODENAME=${MONIKER:-nonamenode}
+    heimdalld init
     
     cd $HEIMDALLD_HOME/config
 
@@ -19,6 +21,7 @@ then
     wget https://raw.githubusercontent.com/maticnetwork/launch/master/mainnet-v1/without-sentry/heimdall/config/genesis.json
 
     echo "overwriting toml config lines"
+    sed -i "s/^moniker.*/moniker = \"$NODENAME\"]/" config.toml
     sed -i "s/^cors_allowed_origins.*/cors_allowed_origins = [\"*\"]/" config.toml
     sed -i 's/^seeds.*/seeds = "f4f605d60b8ffaaf15240564e58a81103510631c@159.203.9.164:26656,4fb1bc820088764a564d4f66bba1963d47d82329@44.232.55.71:26656/"' config.toml
     sed -i "s/^bor_rpc_url.*/bor_rpc_url = \"http://$bor:8540\"/" heimdall-config.toml
