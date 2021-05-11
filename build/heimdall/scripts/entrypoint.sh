8097046b573e582d7823d4f86366ec1643c25c7a
Andrew Redden <opensource@andrewredden.com>
2021-05-11 14:17:49 -0300

    fix rest server entry

--- build/heimdall/scripts/entrypoint.sh
+++ build/heimdall/scripts/entrypoint.sh
@@ -45,7 +45,7 @@ fi
 
 if [ -n "$REST_SERVER" ];
 then
-  EXEC="heimdalld rest-server --moniker=$MONIKER --chain-id=137 --laddr=tcp://0.0.0.0:1317 --max-open=1000 --node=tcp://heimdalld:26657 --trust-node=true"
+  EXEC="heimdalld rest-server --chain-id=137 --laddr=tcp://0.0.0.0:1317 --max-open=1000 --node=tcp://heimdalld:26657 --trust-node=true"
 else
   EXEC="heimdalld start --moniker=$MONIKER --fast_sync --p2p.laddr=tcp://0.0.0.0:26656 --p2p.upnp=false --pruning=syncable --rpc.laddr=tcp://0.0.0.0:26657 --with-tendermint=true"
 fi
