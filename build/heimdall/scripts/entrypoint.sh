68bbfd9cdf6f9d6a18a6c221c957a324dcb9ac0d
MysticRyuujin <mysticryuujin@protonmail.com>
2022-01-25 15:18:03 -0600

    Update seeds

--- build/heimdall/scripts/entrypoint.sh
+++ build/heimdall/scripts/entrypoint.sh
@@ -23,7 +23,7 @@ then
     # CORS
     sed -i "s#^cors_allowed_origins.*#cors_allowed_origins = [\"*\"]#" config.toml
     # SEEDS
-    sed -i "s#^seeds.*#seeds = \"${BOOTNODES:-"f4f605d60b8ffaaf15240564e58a81103510631c@159.203.9.164:26656,4fb1bc820088764a564d4f66bba1963d47d82329@44.232.55.71:26656"}\"#" config.toml
+    sed -i "s#^seeds.*#seeds = \"${BOOTNODES:-"f4f605d60b8ffaaf15240564e58a81103510631c@159.203.9.164:26656,4fb1bc820088764a564d4f66bba1963d47d82329@44.232.55.71:26656,2eadba4be3ce47ac8db0a3538cb923b57b41c927@35.199.4.13:26656,3b23b20017a6f348d329c102ddc0088f0a10a444@35.221.13.28:26656,25f5f65a09c56e9f1d2d90618aa70cd358aa68da@35.230.116.151:26656"}\"#" config.toml
     # heimdall-config.toml
     # BOR
     sed -i "s#^bor_rpc_url.*#bor_rpc_url = \"http://bor:8545\"#" heimdall-config.toml
